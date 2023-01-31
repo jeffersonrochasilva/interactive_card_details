9d934f32a4f58e54d4ab616e63c1d234e68fa3c1
DaviAugustoMS     <jeffersonrochadasilva667@gmail.com>
2023-01-31 11:14:40 -0300

    card interativo vue

---
+++ src/components/Form/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-form class="form">
+    <div>
+      <p class="form__title">CARDHOLDER NAME</p>
+      <input
+        class="form__camp"
+        v-model="name"
+        placeholder="e.g Jane Appleseed"
+      />
+    </div>
+    <div>
+      <p class="form__title">CARD NUMBER</p>
+      <input
+        class="form__camp"
+        v-model="numCard"
+        placeholder="e.g 1234 5678 9123 0000"
+      />
+    </div>
+    <div class="form__boxContent">
+      <div>
+        <p class="form__boxContent__title">EXP. DATA</p>
+        <input class="form__boxContent__camp" v-model="mm" placeholder="MM" />
+      </div>
+      <div>
+        <p class="form__boxContent__title">(MM/YY)</p>
+        <input class="form__boxContent__camp" v-model="yy" placeholder="YY" />
+      </div>
+      <div>
+        <p class="form__boxContent__title">CVC</p>
+        <input
+          class="form__boxContent__campAltern"
+          v-model="cvc"
+          placeholder="e.g 123"
+        />
+      </div>
+    </div>
+    <v-btn flat class="form__btn">confirm</v-btn>
+  </v-form>
+</template>
+<script lang="ts" setup>
+import { Ref, ref } from "vue";
+
+const name: Ref<string> = ref("");
+const numCard: Ref<string> = ref("");
+const mm: Ref<string> = ref("");
+const yy: Ref<string> = ref("");
+const cvc: Ref<string> = ref("");
+</script>
+
+<style lang="scss" scoped>
+.form {
+  width: 25%;
+  height: 400px;
+  padding: 25px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  &__title {
+    font-family: sans-serif;
+    font-weight: bold;
+    color: #31074e;
+    font-size: 14px;
+    margin-bottom: 7px;
+  }
+  &__camp {
+    height: 40px;
+    width: 100%;
+    border: 2px solid rgba(128, 128, 128, 0.192);
+    border-radius: 4px;
+    padding: 0 4px;
+  }
+  &__boxContent {
+    display: flex;
+    justify-content: space-between;
+    &__title {
+      font-family: sans-serif;
+      font-weight: bold;
+      color: #31074e;
+      font-size: 14px;
+      margin-bottom: 7px;
+    }
+    &__camp {
+      height: 40px;
+      width: 90px;
+      border: 2px solid rgba(128, 128, 128, 0.192);
+      border-radius: 4px;
+      padding: 0 4px;
+    }
+    &__campAltern {
+      height: 40px;
+      width: 180px;
+      border: 2px solid rgba(128, 128, 128, 0.192);
+      border-radius: 4px;
+      padding: 0 4px;
+    }
+  }
+  &__btn {
+    background: #31074e;
+    color: #fff;
+    width: 100%;
+    height: 60px !important;
+    border-radius: 7px;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <div class="home__left"></div>
+    <div class="home__rigth">
+      <div class="home__rigth__cards">
+        <div class="home__rigth__cards__cardColor"></div>
+        <div class="home__rigth__cards__cardWhite"></div>
+      </div>
+      <div class="home__rigth__form">
+        <Form />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Form from "@/components/Form/index.vue";
+</script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100vh;
+  input::placeholder {
+    color: rgba(128, 128, 128, 0.363);
+  }
+  &__left {
+  }
+  &__rigth {
+    &__form {
+    }
+  }
+}
+</style>
